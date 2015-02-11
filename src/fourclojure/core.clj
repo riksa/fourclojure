387a3b5a470ddaec28c03c75a61f90d0da85de0a
riku salkia <riku.salkia@symbio.com>
2015-02-11 12:30:13 +0200

    test for Problem 33, format source using IDEA/Cursive

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -9,42 +9,46 @@
 ;You tripped the alarm! def is bad!
 ;replace "(defn" with "(fn" and remove docline "..."
 (defn p26
-	"Write a function which returns the first X fibonacci numbers."
-	( [x] (p26 [1 1] x))
-	( [s x] 
-		(loop [seq s num x]
-			(if (< num 3) seq
-				(recur (conj seq (reduce + (take-last 2 seq))) (- num 1))
+  "Write a function which returns the first X fibonacci numbers."
+  ([x] (p26 [1 1] x))
+  ([s x]
+    (loop [seq s num x]
+      (if (< num 3) seq
+                    (recur (conj seq (reduce + (take-last 2 seq))) (- num 1))
 
-		))))
+                    ))))
 
 ;You tripped the alarm! def is bad!
 ;replace "(defn" with "(fn" and remove docline "..."
 (defn p27
-	"Write a function which returns true if the given sequence is a palindrome."
-	( [s] (if (string? s) (p27 (apply str (reverse s)) s ) (p27 s (reverse s))))
-	( [s r] (= s r)))
+  "Write a function which returns true if the given sequence is a palindrome."
+  ([s] (if (string? s) (p27 (apply str (reverse s)) s) (p27 s (reverse s))))
+  ([s r] (= s r)))
 
 (defn p28
-	"Write a function which flattens a sequence."
-	( [seq] (if (coll? seq) (reduce concat (map p28 seq)) (list seq))))
+  "Write a function which flattens a sequence."
+  ([seq] (if (coll? seq) (reduce concat (map p28 seq)) (list seq))))
 
 (defn p29
-	"Write a function which takes a string and returns a new string containing only the capital letters."
-	([s] (apply str (filter #(and (<= (int %) (int \Z))(>= (int %) (int \A))) s))))
+  "Write a function which takes a string and returns a new string containing only the capital letters."
+  ([s] (apply str (filter #(and (<= (int %) (int \Z)) (>= (int %) (int \A))) s))))
 
 (defn p30
-	"Write a function which removes consecutive duplicates from a sequence."
-	([s] (reduce p30 [] s))
-	([a b] (if (= (last a) b ) a (conj a b))))
+  "Write a function which removes consecutive duplicates from a sequence."
+  ([s] (reduce p30 [] s))
+  ([a b] (if (= (last a) b) a (conj a b))))
 
 (defn p31
-	"Write a function which packs consecutive duplicates into sub-lists."
-	([s] (p31 [] s))
-	([r s] (loop [resp r seq s]
-			(if (empty? seq) resp
-				(recur (conj resp (first (split-with (partial = (first seq)) seq))) (last (split-with (partial = (first seq)) seq)))))))
+  "Write a function which packs consecutive duplicates into sub-lists."
+  ([s] (p31 [] s))
+  ([r s] (loop [resp r seq s]
+           (if (empty? seq) resp
+                            (recur (conj resp (first (split-with (partial = (first seq)) seq))) (last (split-with (partial = (first seq)) seq)))))))
 
 (defn p32
-	"Write a function which duplicates each element of a sequence."
-	([s] (interleave s s)))
+  "Write a function which duplicates each element of a sequence."
+  ([s] (interleave s s)))
+
+(defn p33
+  "Write a function which replicates each element of a sequence a variable number of times."
+  ([s n] nil ))
--- test/fourclojure/core_test.clj
+++ test/fourclojure/core_test.clj
@@ -4,96 +4,111 @@
 
 (deftest p26test
   (testing "p26"
-    (is 
-    	(= '(1 1 2) (p26 3))
-    )
-    (is 
-		(= '(1 1 2 3 5 8) (p26 6))    
-	)
-    (is 
-		(= '(1 1 2 3 5 8 13 21) (p26 8))
-    )
-  ))
+    (is
+      (= '(1 1 2) (p26 3))
+      )
+    (is
+      (= '(1 1 2 3 5 8) (p26 6))
+      )
+    (is
+      (= '(1 1 2 3 5 8 13 21) (p26 8))
+      )
+    ))
 
 (deftest p27test
   (testing "p27"
-    (is 
-		(false? (p27 '(1 2 3 4 5)))
-    )
-    (is 
-		(true? (p27 "racecar"))
-	)
     (is
-		(true? (p27 [:foo :bar :foo]))
-	)
-    (is 
-    	(true? (p27 '(1 1 3 3 1 1)))
-	)
-    (is 
-    	(false? (p27 '(:a :b :c)))
-    )
-  ))
+      (false? (p27 '(1 2 3 4 5)))
+      )
+    (is
+      (true? (p27 "racecar"))
+      )
+    (is
+      (true? (p27 [:foo :bar :foo]))
+      )
+    (is
+      (true? (p27 '(1 1 3 3 1 1)))
+      )
+    (is
+      (false? (p27 '(:a :b :c)))
+      )
+    ))
 
 (deftest p28test
   (testing "p28"
-    (is 
-		(= (p28 '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
-    )
-    (is 
-    	(= (p28 ["a" ["b"] "c"]) '("a" "b" "c"))
-    )
-    (is 
-    	(= (p28 '((((:a))))) '(:a))
-    )
-))
+    (is
+      (= (p28 '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
+      )
+    (is
+      (= (p28 ["a" ["b"] "c"]) '("a" "b" "c"))
+      )
+    (is
+      (= (p28 '((((:a))))) '(:a))
+      )
+    ))
 
 (deftest p29test
   (testing "p29"
-    (is 
-		(= (p29 "HeLlO, WoRlD!") "HLOWRD")
-    )
     (is
-		(empty? (p29 "nothing"))    	
-	)
-	(is
-		(= (p29 "$#A(*&987Zf") "AZ")		
-	)
-))
+      (= (p29 "HeLlO, WoRlD!") "HLOWRD")
+      )
+    (is
+      (empty? (p29 "nothing"))
+      )
+    (is
+      (= (p29 "$#A(*&987Zf") "AZ")
+      )
+    ))
 
 (deftest p30test
   (testing "p30"
-    (is 
-		(= (apply str (p30 "Leeeeeerrroyyy")) "Leroy")
-    )
     (is
-		(= (p30 [1 1 2 3 3 2 2 3]) '(1 2 3 2 3))
-	)
-	(is
-		(= (p30 [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2]))
-	)
-))
+      (= (apply str (p30 "Leeeeeerrroyyy")) "Leroy")
+      )
+    (is
+      (= (p30 [1 1 2 3 3 2 2 3]) '(1 2 3 2 3))
+      )
+    (is
+      (= (p30 [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2]))
+      )
+    ))
 
 (deftest p31test
   (testing "p31"
-    (is 
+    (is
       (= (p31 [1 1 2 1 1 1 3 3]) '((1 1) (2) (1 1 1) (3 3)))
-    )
-    (is 
+      )
+    (is
       (= (p31 [:a :a :b :b :c]) '((:a :a) (:b :b) (:c)))
-    )
-    (is 
+      )
+    (is
       (= (p31 [[1 2] [1 2] [3 4]]) '(([1 2] [1 2]) ([3 4])))
-    )
-))
+      )
+    ))
 
 (deftest p32test
-	(testing "p32"
-		(is 
-			(= (p32 [1 2 3]) '(1 1 2 2 3 3)))
-		(is 
-			(= (p32 [:a :a :b :b]) '(:a :a :a :a :b :b :b :b)))
-		(is 
-			(= (p32 [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4])))
-		(is 
-			(= (p32 [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4])))
-))
+  (testing "p32"
+    (is
+      (= (p32 [1 2 3]) '(1 1 2 2 3 3)))
+    (is
+      (= (p32 [:a :a :b :b]) '(:a :a :a :a :b :b :b :b)))
+    (is
+      (= (p32 [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4])))
+    (is
+      (= (p32 [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4])))
+    ))
+
+(deftest p33test
+  "Write a function which replicates each element of a sequence a variable number of times."
+  (testing "p33"
+    (is
+      (= (p33 [1 2 3] 2) '(1 1 2 2 3 3)))
+    (is
+      (= (p33 [:a :b] 4) '(:a :a :a :a :b :b :b :b)))
+    (is
+      (= (p33 [4 5 6] 1) '(4 5 6)))
+    (is
+      (= (p33 [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4])))
+    (is
+      (= (p33 [44 33] 2) [44 44 33 33]))
+    ))
