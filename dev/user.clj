35e29381c4ca0bf7a756d542e189959c1dc17adf
riku salkia <riksa@iki.fi>
2015-02-24 19:05:17 +0200

    pimp my repl more

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,22 @@
 (ns user
+  (:require [clojure.tools.namespace.repl :refer [refresh]])
   (:require [fourclojure.core :refer :all]))
 
 ;http://dev.solita.fi/2014/03/18/pimp-my-repl.html
 
 (defn my-repl-util []
   (println "Hello from 'dev'"))
+
+(defn start
+  "Start the application"
+  []
+  nil)
+
+(defn stop
+  "Stop the application"
+  []
+  nil)
+
+(defn reset []
+  (stop)
+  (refresh :after 'user/start))
