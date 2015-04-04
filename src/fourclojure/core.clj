3898d81078733f4ac3f5fbeadc51e3cbdabbd1f6
riku salkia <riksa@iki.fi>
2015-04-04 13:24:31 +0300

    Problem 95: Parentheses... Again

--- src/fourclojure/core.clj
+++ src/fourclojure/core.clj
@@ -424,4 +424,25 @@ Write a function which returns the nth row of Pascal's Triangle. "
        (for [x (dist1-filtered n r)] (p82 x (disj r x) (conj s n)))
        (empty? r)))))
 
+(defn p95
+  "In a family of languages like Lisp, having balanced parentheses is a defining feature of the language. Luckily, Lisp
+  has almost no syntax, except for these 'delimiters' -- and that hardly qualifies as 'syntax', at least in any useful
+  computer programming sense.
+
+  It is not a difficult exercise to find all the combinations of well-formed parentheses if we only have N pairs to work
+  with. For instance, if we only have 2 pairs, we only have two possible combinations: ' () () ' and ' (()) '. Any other
+  combination of length 4 is ill-formed. Can you see why?
+
+  Generate all possible combinations of well-formed parentheses of length 2n (n pairs of parentheses). For this problem,
+  we only consider '(' and ')', but the answer is similar if you work with only {} or only [].
+
+  There is an interesting pattern in the numbers!"
+  ([n] (into #{} (p95 [] n 0)))
+  ([s r o] (if (and (zero? r) (zero? o))
+             (vector (apply str s))
+             (concat
+               (if (> o 0)
+                 (p95 (conj s ")") r (dec o)))
+               (if (> r 0)
+                 (p95 (conj s "(") (dec r) (inc o)))))))
 
